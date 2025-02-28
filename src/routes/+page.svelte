55cfc9bd9013ae5cbc46692f5d655961b857e109
Fred_Dead_Redemption <frederikdau@gmail.com>
2025-02-28 13:19:42 +0100

    sass fuckery

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit<p>f</p></h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style lang="scss">
+  h1{
+    color: $primary;
+    z-index: 10000;
+    p{
+      color: blue;
+    }
+  }
+</style>
