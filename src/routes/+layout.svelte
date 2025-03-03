c0775af728b05aaf7500c31fb9df0a06c7f7846c
Fred_Dead_Redemption <frederikdau@gmail.com>
2025-03-03 17:02:05 +0100

    nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,28 +26,37 @@
 
 <nav id="nav">
   <a href="/">Home</a>
-  <button class="logout" onclick={logout}>Logout</button>
+  {#if session}
+    <button class="auth" onclick={logout}>Logout</button>
+  {:else}
+    <a class="auth" href="/auth">login</a>
+  {/if}
 </nav>
 
 {@render children()}
 
 <style lang="scss">
   #nav{
+    position: absolute;
     display: flex;
     flex-direction: row;  
     align-items: center;
     justify-content: space-between;
     height: 80px;
-    width: 100vw;
+    margin: 10px;
+    width: calc(100% - 20px);
+    left: 50%;
+    transform: translateX(calc(-50% - 10px));
     padding: 25px;
     gap: 10px;
     background-color: $grey-mid;
     box-shadow: $box-shadow-primary;
-    .logout{
+    .auth{
       border: 1px solid crimson;
       background-color: rgb(224, 64, 96);
       color: $white;
       padding: 15px;
+      text-decoration: none;
       &:hover{
         cursor: pointer;
         background-color: crimson;
