5ebb197680a8c6852399d2671356c499ea23047e
Fred_Dead_Redemption <frederikdau@gmail.com>
2025-03-11 00:48:21 +0100

    style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 
 <div class="content">
   <form method="POST" action="?/login" use:enhance={() => {
-		return async ({ result }: any) => {
+    return async ({ result }: any) => {
       console.log(result);
       if(result.type = "redirect"){
         goto(result.location);
@@ -16,8 +16,8 @@
       if(!result?.data?.success){
         errmsg = result?.data?.message;
       } 
-		};
-	}}>
+    };
+  }}>
     <input name="email" type="email" placeholder="EMAIL" />
     <input name="password" type="password" placeholder="PASSWORD" />
     <button class="login">LOGIN</button>
@@ -48,25 +48,37 @@
     transform: translate(-50%, -50%);
     width: 300px;
   }
+  input:-webkit-autofill,
+input:-webkit-autofill:hover, 
+input:-webkit-autofill:focus,
+textarea:-webkit-autofill,
+textarea:-webkit-autofill:hover,
+textarea:-webkit-autofill:focus,
+select:-webkit-autofill,
+select:-webkit-autofill:hover,
+select:-webkit-autofill:focus {
+  -webkit-text-fill-color: $primary;
+  -webkit-box-shadow: 0 0 0px 1000px transparent inset;
+  transition: background-color 5000s ease-in-out 0s;
+}
   form{
     display: flex;
     flex-direction: column;
-    border-radius: 4px;
     padding: 25px;
     gap: 25px;
-    background-color: rgba($color: $white, $alpha: 0.2);
+    background-color: $white;
     backdrop-filter: blur(5px);
-    border: 1px solid $white;
+    border: 1px solid $grey-mid;
     input, button{
       width: 250px;
       padding: 10px;
-      border-radius: 4px;
     }
     input{  
-      border: 1px solid $white;
-      background-color: transparent;  
+      border: none;
+      border: 1px solid $grey-mid;
+      background-color: $grey-ultralight;  
       outline: none;
-      color: $white;
+      color: $grey-mid;
     }
     button{
       border: none;
@@ -77,10 +89,20 @@
     }
   }
   .login{
-    background-color: $ok;
+    transition: 250ms ease all;
+    background-color: $primary;
     color: $white;
+    &:hover{
+      background-color: $black;
+    }
   }
   .create{
+    transition: 250ms ease all;
+    border: 1px solid $grey-mid;
     background-color: $white;
+    &:hover{
+      background-color: $grey-mid;
+      color: $white;
+    }
   }
 </style>
