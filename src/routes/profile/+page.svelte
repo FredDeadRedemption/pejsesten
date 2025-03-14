efad2fc6a63138cdb136f19d374ee29706481eb1
Fred_Dead_Redemption <frederikdau@gmail.com>
2025-03-14 15:11:44 +0100

    username changing

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,7 +1,52 @@
-<!-- src/routes/account/+page.svelte -->
 <script lang="ts">
+	import { enhance } from '$app/forms';
+	import { onMount } from 'svelte';
+
    let { data } = $props()
    let { profile } = $derived(data)
-</script>
+   let username: any = $state("");
+   let originalUsername: any = $state("");
 
-<div>{profile?.username}</div>
+   onMount(()=>{
+      username = profile?.username
+      originalUsername = profile?.username
+   })
+ </script>
+ 
+ <main>
+   <form use:enhance={() => {
+      return ({ result }) => {
+         if(!result) return;
+         if (result.type === 'success') {  
+            username = result?.data?.username;
+            originalUsername = result?.data?.username;
+         }
+      };
+    }} action="?/update" method="post">
+     <label for="username">Hello there, <b>{originalUsername}</b>, brave adventurer! Forge your destiny and customize your profile here.</label>
+     <input type="text" id="username" name="username" bind:value={username} required />
+     <button class="button primary" type="submit">Update</button>
+   </form>
+ </main>
+ 
+ <style lang="scss">
+   main {
+      max-width: 400px;
+      margin: 20px auto;
+      text-align: center;
+   }
+   input {
+      text-align: center;
+      font-size: 2rem;
+      background-color: transparent;
+      width: 100%;
+      padding: 8px;
+      margin: 10px 0;
+      border: none;
+      border-bottom: 1px solid $grey-mid;
+   }
+   button{
+      width: 100% !important;
+   }
+ </style>
+ 
