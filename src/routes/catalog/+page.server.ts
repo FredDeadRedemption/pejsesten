0ba467e2898a9bc2867c6c77e0f8da0590d58b0a
Fred_Dead_Redemption <frederikdau@gmail.com>
2025-03-14 22:34:34 +0100

    card shii

--- src/routes/catalog/+page.server.ts
+++ src/routes/catalog/+page.server.ts
@@ -1,9 +1,15 @@
 import type { PageServerLoad } from '../catalog/$types'
+import type { Database } from '$lib/database.types'; 
+type LandEnum = Database['public']['Tables']['land_enums']['Row'];
+type Card = Database['public']['Tables']['cards']['Row'];
 
 export const load: PageServerLoad = async ({ locals: { supabase } }) => {
   const { data: land_enums } = await supabase.from('land_enums').select('*');
 
   const { data: cards } = await supabase.from("cards").select("*");
 
-  return { land_enums: land_enums ?? [], cards: cards ?? [] }
+  const typedLandEnums = land_enums as LandEnum[] | null;
+  const typedCards = cards as Card[] | null;
+
+  return { land_enums: typedLandEnums ?? [], cards: typedCards ?? [] }
 }
