6dca376e91d32d9099b4da5fade89f24c513130e
Fred_Dead_Redemption <frederikdau@gmail.com>
2025-03-17 14:09:58 +0100

    yehaw

--- src/lib/socket/socket.ts
+++ src/lib/socket/socket.ts
@@ -11,18 +11,6 @@ export const socketLabel = socketURL === p ? "Production" : "Development"
 const socket = io(socketURL);
 console.log("CREATING SOCKET SERVER")
 
-export const messages = writable<string[]>([]);
-
-// Listen for messages
-socket.on("message", (data) => {
-  messages.update((msgs) => [...msgs, data]);
-});
-
-// Function to send a message
-export const sendMessage = (message: string) => {
-  socket.emit("message", message);
-}
-
 // Queue up
 export const queueUp = (userName: string) => {
   socket.emit("queueUp", userName);
--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { sendMessage, queueUp, messages, socketLabel } from "$lib/socket/socket";
+  import { queueUp, socketLabel } from "$lib/socket/socket";
 
   let { profile } = $props();
 
@@ -9,34 +9,15 @@
 <main>
   <h1>Socket.IO Chat</h1>
   <p>Runnin on <strong>{socketLabel}</strong> change in <a href="/settings">settings</a></p>
-  <div>
-    <input
-    type="text"
-    bind:value={$messages}
-    placeholder="Type a message"
-    onkeydown={(e) => e.key === 'Enter' && sendMessage(message)}
-  />
-  <button onclick={() => { sendMessage(message); message = ""; }}>
-    Send
-  </button>
-  </div>
 
-  <button onclick={() => { queueUp("mista yehaw") }}>
+  <button class="button primary" onclick={() => { queueUp("mista yehaw") }}>
     QueueUp
   </button>
-  <ul>
-    {#each $messages as msg}
-      <li class="msg">{msg}</li>
-    {/each}
-  </ul>
 </main>
 
 <style lang="scss">
   a{
     color: $primary;
-  }
-  .msg{
-    width: 500px;
   }
   main {
     display: flex;
@@ -46,19 +27,5 @@
     text-align: center;
     margin-top: 2rem;
     gap: 10px;
-  }
-  input {
-    padding: 0.5rem;
-    margin-right: 0.5rem;
-  }
-  ul {
-    list-style: none;
-    padding: 0;
-  }
-  li {
-    background: #f0f0f0;
-    padding: 0.5rem;
-    margin: 0.25rem 0;
-    border-radius: 5px;
   }
 </style>
