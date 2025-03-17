5a1cfc3b458ce9d275461f8a31855b2c4c181028
Fred_Dead_Redemption <frederikdau@gmail.com>
2025-03-17 20:42:51 +0100

    Update +layout.ts

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -31,13 +31,9 @@ export const load: LayoutLoad = async ({ data, depends, fetch }) => {
    * safe, and on the server, it reads `session` from the `LayoutData`, which
    * safely checked the session using `safeGetSession`.
    */
-  const {
-    data: { session },
-  } = await supabase.auth.getSession()
+  const { data: { session }} = await supabase.auth.getSession()
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
+  const { data: { user }} = await supabase.auth.getUser()
 
   const profile = data?.profile;
 
