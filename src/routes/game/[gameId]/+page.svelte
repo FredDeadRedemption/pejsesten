b334e0030884aa858901d3e07da7cc6ab1cecef7
Fred_Dead_Redemption <frederikdau@gmail.com>
2025-03-19 13:35:42 +0100

    Update +page.svelte

--- src/routes/game/[gameId]/+page.svelte
+++ src/routes/game/[gameId]/+page.svelte
@@ -2,11 +2,35 @@
    import { endTurn } from "$lib/socket/socket";
 
   import { page } from "$app/state";
+	import { browser } from "$app/environment";
   let gameId = $state(page.params.gameId);
+
+  function enterFullscreen() {
+    if(!browser) return;
+    const elem = document.getElementById("fullscreen-div");
+    if (elem?.requestFullscreen) {
+      elem.requestFullscreen();
+    }
+  }
 </script>
 
-<!-- svelte-ignore a11y_consider_explicit_label -->
-<button class="button primary" onclick={()=>{
-  endTurn();
-}}>END TURN</button>
-<h1>Game ID: {gameId}</h1>
+<div id="fullscreen-div">
+  <!-- svelte-ignore a11y_consider_explicit_label -->
+  <button class="button primary" onclick={()=>{
+    endTurn();
+  }}>END TURN</button>
+  <h1>Game ID: {gameId}</h1>
+</div>
+<button onclick={()=> {enterFullscreen()}}>Go Fullscreen</button>
+
+<style lang="scss">
+  #fullscreen-div {
+    width: 100%;
+    height: 100%;
+    background-color: lightgreen;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 2rem;
+  }
+</style>
