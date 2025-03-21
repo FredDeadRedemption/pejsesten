ccd8a9187a50ba0691798744ac7ed354f06bed1e
Fred_Dead_Redemption <frederikdau@gmail.com>
2025-03-21 18:19:03 +0100

    deckbuilder

--- src/routes/deck-builder/+page.svelte
+++ src/routes/deck-builder/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import Card from '$lib/components/card.svelte';
+  import type { Database } from '$lib/database.types'; 
+	import { slide } from 'svelte/transition';
+  type Card = Database['public']['Tables']['cards']['Row'];
 
   let { data } = $props()
   let { cards } = $derived(data);
@@ -13,50 +16,62 @@
     )
   );
 
-  type DeckCard = {
-    id: number,
+  type CardAndCount = {
+    card: Card,
     count: number,
-    name: string,
-    image_url: string
   }
 
-  let deck: number[] = $state([]);
-  let deckMetaData: DeckCard[] = $state([]);
+  let deck: CardAndCount[] = $state([]);
+
+  let totalDeckCount = $derived(
+    deck.reduce((total, cardAndCount) => total + cardAndCount.count, 0)
+  );
 </script>
 
-<input type="text" name="search" id="" bind:value={searchTerm}>
-<h1>{deck.length + " / 30"}</h1>
-<form action="?/createDeck" method="POST" use:enhance>
-  <input style="display: none;" type="text" name="deck" bind:value={deck}>
-  <button class="button primary" type="submit">CREATE DECK</button>
-  </form>
 <div class="main">
-  <div class="card-wrapper">
-    {#each filteredCards as card}
-       <!-- svelte-ignore a11y_consider_explicit_label -->
-      <button class="invisible" onclick={()=>{
-        if (!deck){ deck = [] }; // form submission makes deck null? so redefine it
-        if(deck.filter(cardId => cardId === card.id).length >= 3) return;
-        deck.push(card.id)
+  <div class="catalog-search-wrapper">
+    <input type="text" name="search" id="" bind:value={searchTerm}>
+    <h1>{totalDeckCount + " / 30"}</h1>
+    <form action="?/createDeck" method="POST" use:enhance>
+      <input style="display: none;" type="text" name="deck" bind:value={deck}>
+      <button class="button primary" type="submit">CREATE DECK</button>
+    </form>
+    <div class="card-wrapper">
+      {#each filteredCards as card}
+         <!-- svelte-ignore a11y_consider_explicit_label -->
+        <button class="invisible" onclick={()=>{
+          if (!deck){ deck = [] }; // form submission makes deck null? so redefine it
+          if(deck.length >= 30) return;
+          
+          const existingCard = deck.find(c => c.card.id === card.id);
+          if (existingCard && existingCard?.count >= 2) return;
 
-        const existingCard = deckMetaData.find(deckCard => deckCard.id === card.id);
-        if (existingCard) {
-          existingCard.count += 1;
-        } else {
-          deckMetaData.push({ id: card.id, count: 1, name: card.name, image_url: card.image_url });
-        }
-      }}>
-        <Card card={card}></Card>
-      </button>
-    {/each}
+          if (existingCard) {
+            existingCard.count++;
+          } else {
+            deck.push({ card: card, count: 1})
+          }
+        }}>
+          <Card card={card}></Card>
+        </button>
+      {/each}
+    </div>
   </div>
   <div class="deck">
-    {#each deckMetaData as card}
-    <div class="card-in-deck-view">
-      <!-- <img src={card.image_url} alt=""> -->
-      <!-- <div class="card-in-deck-content"> -->
-        <p>{card.id}</p><p> COUNT: {card.count}</p> 
-      <!-- </div> -->
+    {#each deck as cardAndCount}
+    <!-- svelte-ignore a11y_click_events_have_key_events -->
+    <!-- svelte-ignore a11y_no_static_element_interactions -->
+    <div transition:slide={{ axis: "x", duration: 250 }} class="card-in-deck-view" style="background-image: url({cardAndCount.card.image_url});" onclick={()=>{
+      const cardIndex = deck.findIndex((c) => c.card.id === cardAndCount.card.id);
+        if (cardIndex !== -1) {
+          if(deck[cardIndex].count > 1){
+            deck[cardIndex].count--;
+          } else {
+            deck.splice(cardIndex, 1);
+          }
+        }
+    }}>
+      <span class="name">{cardAndCount.card.name}</span><span class="count">x {cardAndCount.count}</span>
     </div>
     {/each}
   </div>
@@ -70,24 +85,44 @@
     overflow: hidden;
     display: flex;
     flex-wrap: wrap;    
-    max-height: 450px;
+    max-height: 500px;
     flex-direction: column;
     .card-in-deck-view{
       display: flex;
-      justify-content: center;
+      justify-content: space-between;
       align-items: center;
       height: 40px;
       width: 100%;
-      img{
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
+      background-size: cover;
+      color: $white;
+      user-select: none; 
+      font-weight: 800;
+      &:hover{
+        cursor: pointer;
       }
-    }
-    .card-in-deck-content{
-      position: absolute;
-      height: 100%;
-      width: 100%;
+      .name{
+        display: flex;
+        align-items: center;
+        white-space: nowrap;
+        flex-grow: 1;
+        padding-left: 10px;
+        height: 100%;
+        background: linear-gradient(to right, 
+        rgb(0, 0, 0) 0%, 
+        rgba(31, 31, 31, 0)100%);
+      }
+      .count{
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        white-space: nowrap;
+        width: 50px;
+        padding-right: 10px;
+        height: 100%;
+        background: linear-gradient(to left, 
+        rgb(0, 0, 0) 0%, 
+        rgba(31, 31, 31, 0)100%);
+      }
     }
   }
   .invisible{
