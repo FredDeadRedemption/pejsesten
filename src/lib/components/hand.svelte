99171ec55a43be1e8f84df8207d54e689f4f77f7
Fred_Dead_Redemption <frederikdau@gmail.com>
2025-03-24 20:31:43 +0100

    Update hand.svelte

--- src/lib/components/hand.svelte
+++ src/lib/components/hand.svelte
@@ -7,55 +7,39 @@
   };
 
   const oddRotationMap: Map = {
-    0: "-30deg",  // Far left
-    1: "-20deg",
-    2: "-10deg",
-    3: "-5deg",
-    4: "0deg",     // Perfectly straight middle card
-    5: "5deg",
-    6: "10deg",
-    7: "20deg",
-    8: "30deg"    // Far right
+    0: "-30deg", 1: "-20deg", 2: "-10deg", 3: "-5deg",
+    4: "0deg", // center
+    5: "5deg", 6: "10deg", 7: "20deg", 8: "30deg"   
   };
 
-  // For even number of cards (2, 4, 6, 8, 10)
   const evenRotationMap: Map = {
-    0: "-30deg",   // Far left
-    1: "-20deg",
-    2: "-12deg",
-    3: "-6deg",
-    4: "-3deg",    // Left center (no perfect middle)
-    5: "3deg",     // Right center
-    6: "6deg",
-    7: "12deg",
-    8: "20deg",
-    9: "30deg"     // Far right
+    0: "-30deg", 1: "-20deg", 2: "-12deg", 3: "-6deg",
+    4: "-3deg", 5: "3deg", // center pair
+    6: "6deg", 7: "12deg", 8: "20deg", 9: "30deg"  
   };
 
-  // Position maps with overlapping
-  const oddPositionMap: Map = { // For 1,3,5,7,9 cards
-    8: "-70px",   // Furthest left
-    7: "-50px",   // Less overlap near center
-    6: "-30px",
-    5: "-15px",
-    4: "0px",     // Center card
-    3: "15px",
-    2: "30px",
-    1: "50px",
-    0: "70px"     // Furthest right
+  const oddPositionMap: Map = { 
+    8: "-70px", 7: "-50px", 6: "-30px", 5: "-15px",
+    4: "0px", // center 
+    3: "15px", 2: "30px", 1: "50px", 0: "70px"     
   };
 
-  const evenPositionMap: Map = { // For 2,4,6,8,10 cards
-    9: "-75px",
-    8: "-55px",
-    7: "-35px",
-    6: "-20px",
-    5: "-10px",   // Left center pair
-    4: "10px",    // Right center pair
-    3: "20px",
-    2: "35px",
-    1: "55px",
-    0: "75px"
+  const evenPositionMap: Map = { 
+    9: "-85px", 8: "-55px", 7: "-35px", 6: "-20px",
+    5: "-10px", 4: "10px", // center pair
+    3: "20px", 2: "35px", 1: "55px", 0: "85px"
+  };
+
+  const oddYPositionMap: Map = {
+    0: "20px", 1: "10px", 2: "4px", 3: "1px",
+    4: "0px",  // center
+    5: "1px", 6: "4px", 7: "10px", 8: "20px"
+  };
+
+  const evenYPositionMap: Map = {
+    0: "18px", 1: "9px", 2: "4px", 3: "1px",
+    4: "0px", 5: "0px", // center pair
+    6: "1px", 7: "4px", 8: "9px", 9: "18px"
   };
 
   function getMappedValues(index: number) {
@@ -66,7 +50,8 @@
     
     return {
       rotation: isEven ? evenRotationMap[virtualIndex] : oddRotationMap[virtualIndex],
-      position: isEven ? evenPositionMap[virtualIndex] : oddPositionMap[virtualIndex]
+      position: isEven ? evenPositionMap[virtualIndex] : oddPositionMap[virtualIndex],
+      yPosition: isEven ? evenYPositionMap[virtualIndex] : oddYPositionMap[virtualIndex]
     };
   }
 </script>
@@ -75,7 +60,9 @@
   {#each hand as card, index}
     <div 
       class="cardback" 
-      style="--rotation: {getMappedValues(index).rotation}; --position: {getMappedValues(index).position};"
+      style="--rotation: {getMappedValues(index).rotation}; 
+      --position: {getMappedValues(index).position}; 
+      --y-position: {getMappedValues(index).yPosition}"
     ></div>
   {/each}
 </div>
@@ -105,6 +92,7 @@
     left: 50%;
     transform: 
       translateX(calc(-50% + var(--position)))
+      translateY(calc(var(--y-position) - 40px))
       rotate(var(--rotation));
     z-index: var(--i);
     
@@ -112,6 +100,7 @@
     &:hover {
       transform: 
        translateX(calc(-50% + var(--position)))
+       translateY(calc(var(--y-position) - 40px))
         rotate(var(--rotation))
         translateY(-30px);
       z-index: 100;
