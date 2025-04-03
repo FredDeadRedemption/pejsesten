b1d91456e83217125466bf25bbcb08eb4c181b71
MagnusHoese <magjen22@student.aau.dk>
2025-04-03 16:08:19 +0200

    update on catalog init

--- src/lib/components/cardAdminPanel.svelte
+++ src/lib/components/cardAdminPanel.svelte
@@ -3,7 +3,8 @@
   import { downloadDivAsPNG } from '$lib/util';
   import type { SupabaseClient } from '@supabase/supabase-js';
   import type { Database } from '$lib/database.types'; 
-	import { slide } from 'svelte/transition';
+	import { fade, slide } from 'svelte/transition';
+	import { enhance } from '$app/forms';
   type Card = Database['public']['Tables']['cards']['Row'];
 
   let { card, supabase, onDeleteCard }: { card: Card, supabase: SupabaseClient, onDeleteCard: any} = $props()
@@ -56,6 +57,29 @@
   let deleting: boolean = $state(false);
 
   const beginDelete = () => deleting = true;
+
+  let updating: boolean = $state(false);
+
+
+  const handleImgUpdate = (event : Event) => {
+    const target = event.target as HTMLInputElement;
+
+    const img = target.files?.[0];
+
+    if (img) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        if(card) {
+          card.image_url = e.target?.result as string;
+          console.log(card.image_url)
+        }
+      }
+      reader.readAsDataURL(img);
+
+      // console.log(img);
+    }
+  } 
 </script>
 
 <div id="panel">
@@ -63,7 +87,7 @@
     <button transition:slide={{ axis: "x", duration: 100 }} class="btn download" onclick={() => downloadDivAsPNG(card.name, card.name)}>
       <span class="icon">{@html getIcon("png")}</span>
     </button>
-    <button transition:slide={{ axis: "x", duration: 100 }} class="btn edit" onclick={() => downloadDivAsPNG(card.name, card.name)}>
+    <button transition:slide={{ axis: "x", duration: 100 }} class="btn edit" onclick={() => {updating= !updating}}>
       <span class="icon">{@html getIcon("creator")}</span>
     </button>
   {/if}
@@ -71,6 +95,56 @@
     <span class="icon">{@html getIcon("delete")}</span>
   </button>
 </div>
+
+
+{#if updating}
+<div class="pop-up-wrapper">
+  <button onclick={() => {updating = !updating}}>X</button>
+  <form  method="POST" action="?/updateCard" use:enhance enctype="multipart/form-data">
+    <div class="grp">
+      <label for="name">Name</label>
+      <input type="text" id="name" name="name" required bind:value={card.name}/>
+    </div>
+
+    <div class="grp-side-by-side-four">
+      <div class="grp">
+        <label for="holyCost">Holy</label>
+        <input type="number" id="holyCost" name="holyCost" bind:value={card.holy_cost}/>
+      </div>
+
+      <div class="grp">
+        <label for="deathCost">Death</label>
+        <input type="number" id="deathCost" name="deathCost" bind:value={card.death_cost}/>
+      </div>
+
+      <div class="grp">
+        <label for="dreamCost">Dream</label>
+        <input type="number" id="dreamCost" name="dreamCost" bind:value={card.dream_cost}/>
+      </div>
+
+      <div class="grp">
+        <label for="earthCost">Earth</label>
+        <input type="number" id="earthCost" name="earthCost" bind:value={card.earth_cost}/>
+      </div>
+    </div>
+
+    <div class="grp">
+      <label for="description">Description</label>
+      <textarea id="description" name="description" bind:value={card.description}></textarea>
+    </div>
+
+    <div class="grp">
+      <label for="image">Image</label>
+      <input type="file" id="image" name="image" accept="image/*" onchange={handleImgUpdate} />
+      {console.log(card.image_url)}
+      <img src={card.image_url} alt="" draggable="false">
+    </div>
+    <button type="submit" class="button primary">Update Card</button>
+  </form>
+</div>
+
+<div id="blur" transition:fade={{ duration: 100 }}></div>
+{/if}
 
 
 <style lang="scss">
@@ -105,4 +179,26 @@
   .download:hover { background-color: $blue; color: $white; }
   .delete:hover { background-color: $red; color: $white; }
   .edit:hover { background-color: $yellow; color: $white; }
+
+
+  #blur{
+    z-index: 50;
+    position: fixed;
+    height: 100vh;
+    width: 100vw;
+    top: 0px;
+    left: 0px;
+    background-color: rgba($white, 0.45);
+    backdrop-filter: blur(3px);
+    pointer-events: all;
+  }
+  .pop-up-wrapper {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 9999999;
+    background-color: wheat;
+  }
+  
 </style>
