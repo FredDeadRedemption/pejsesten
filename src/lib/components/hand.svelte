859e3811cf3a21ff5d3981a5a1f1704fbb2c23b1
Fred_Dead_Redemption <frederikdau@gmail.com>
2025-04-04 09:27:49 +0200

    remove battleField element

--- src/lib/components/hand.svelte
+++ src/lib/components/hand.svelte
@@ -9,7 +9,7 @@
 
   let handElement: HTMLElement;
 
-  let { hand = $bindable(), battleFieldElement } = $props();
+  let { hand = $bindable() } = $props();
 
   let hoverIndex: number | null = $state(null); // keeps track of which index to display big card
   let draggerIndex: number | null = $state(null); // keeps track of which index is to hide because it's being dragged
