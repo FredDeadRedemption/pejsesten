5fe6ba7042c70ba7a5fe630259bdbf0198116213
freddeadredemption <fh@finrod.dk>
2026-04-05 23:20:20 +0200

    rm useless shit

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -8,27 +8,11 @@ export const enterFullscreen = (divID: string) => {
 	}
 };
 
-export const getRandomString = (length: number): string => {
-	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	let result = '';
-	const charactersLength = characters.length;
-	for (let i = 0; i < length; i++) {
-		result += characters.charAt(Math.floor(Math.random() * charactersLength));
-	}
-	return result;
-};
-
 export const getRandomDeckName = () => {
 	const names = [
 		"Pixie's Rejected Tarot",
-		"Thoth's Hangover Deck",
 		'Rider-Waite Smackdown',
-		"The Fool's Tax Return",
-		'Tower Moment Deck',
 		'Moonlight Malpractice',
-		'Golden Dawn Interns',
-		"Crowley's Coffee Order",
-		"Satan's To-Do List",
 		'DMT Elf encounter',
 		'Demonology for Dummies'
 	];
