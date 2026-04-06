ce5a4bef5be9dc0384c4066fb3b1c3f30ed17edf
freddeadredemption <fh@finrod.dk>
2026-04-06 13:10:45 +0200

    Update cards.ts

--- src/lib/shared/cards.ts
+++ src/lib/shared/cards.ts
@@ -31,7 +31,7 @@ const cards: Card[] = [
 		color: 'white',
 		name: 'Guy With a Stick',
 		description: 'Nobody knows where he came from. Nobody asked.',
-		baseCost: 2,
+		baseCost: 1,
 		type: 'minion',
 		races: ['human'],
 		baseAttack: 1,
@@ -45,7 +45,7 @@ const cards: Card[] = [
 		color: 'white',
 		name: 'Overzealous Priest',
 		description: 'He will heal you whether you like it or not.',
-		baseCost: 3,
+		baseCost: 2,
 		type: 'minion',
 		races: ['human'],
 		baseAttack: 2,
@@ -59,11 +59,11 @@ const cards: Card[] = [
 		color: 'white',
 		name: 'Silverguard Knight',
 		description: 'Has never actually been in a fight. Looks great though.',
-		baseCost: 4,
+		baseCost: 3,
 		type: 'minion',
 		races: ['human'],
-		baseAttack: 3,
-		baseDefence: 5,
+		baseAttack: 5,
+		baseDefence: 3,
 		image_url: '',
 		abilities: [],
 		attributes: []
@@ -73,7 +73,7 @@ const cards: Card[] = [
 		color: 'white',
 		name: 'Radiant Sentinel',
 		description: 'Light pours from every crack in her armour. Doctors are concerned.',
-		baseCost: 5,
+		baseCost: 4,
 		type: 'minion',
 		races: ['elf'],
 		baseAttack: 4,
@@ -87,7 +87,7 @@ const cards: Card[] = [
 		color: 'white',
 		name: 'Herald of the Sun',
 		description: 'Arrives before the dawn. Leaves before doing the dishes.',
-		baseCost: 6,
+		baseCost: 5,
 		type: 'minion',
 		races: ['elf'],
 		baseAttack: 6,
@@ -101,13 +101,13 @@ const cards: Card[] = [
 	{
 		id: 6,
 		color: 'black',
-		name: 'Duskblade Rogue',
-		description: 'Strikes when the candles go out. Also steals your cutlery.',
-		baseCost: 2,
+		name: 'Black Cat',
+		description: 'Meaw.',
+		baseCost: 1,
 		type: 'minion',
-		races: ['human'],
-		baseAttack: 3,
-		baseDefence: 2,
+		races: ["beast"],
+		baseAttack: 2,
+		baseDefence: 1,
 		image_url: '',
 		abilities: [],
 		attributes: []
@@ -117,7 +117,7 @@ const cards: Card[] = [
 		color: 'black',
 		name: 'Barry the Hexblade',
 		description: 'Every wound he deals festers. His name does not.',
-		baseCost: 3,
+		baseCost: 2,
 		type: 'minion',
 		races: ['human'],
 		baseAttack: 4,
@@ -131,11 +131,11 @@ const cards: Card[] = [
 		color: 'black',
 		name: 'Void Stalker',
 		description: 'Hunts in the space between thoughts. Probably behind you right now.',
-		baseCost: 4,
+		baseCost: 3,
 		type: 'minion',
 		races: ['elf'],
-		baseAttack: 5,
-		baseDefence: 3,
+		baseAttack: 3,
+		baseDefence: 5,
 		image_url: '',
 		abilities: [],
 		attributes: []
@@ -148,8 +148,8 @@ const cards: Card[] = [
 		baseCost: 4,
 		type: 'minion',
 		races: ['human'],
-		baseAttack: 3,
-		baseDefence: 5,
+		baseAttack: 5,
+		baseDefence: 4,
 		image_url: '',
 		abilities: [],
 		attributes: []
@@ -163,7 +163,7 @@ const cards: Card[] = [
 		type: 'minion',
 		races: ['elf'],
 		baseAttack: 7,
-		baseDefence: 4,
+		baseDefence: 6,
 		image_url: '',
 		abilities: [],
 		attributes: []
@@ -174,7 +174,7 @@ const cards: Card[] = [
 		id: 11,
 		color: 'white',
 		name: 'Divine Power',
-		description: 'Wraps an ally in impenetrable holy light. Terms and conditions apply.',
+		description: 'Give a friendly minion +4 +4',
 		baseCost: 2,
 		type: 'incantation',
 		image_url: '',
@@ -201,8 +201,8 @@ const cards: Card[] = [
 		id: 12,
 		color: 'white',
 		name: 'Agnostic Wrath',
-		description: 'Calls down judgement. Not affiliated with any religion.',
-		baseCost: 4,
+		description: 'Deal 4 damage',
+		baseCost: 3,
 		type: 'incantation',
 		image_url: '',
 		abilities: [
@@ -211,11 +211,11 @@ const cards: Card[] = [
 				effects: [
 					{
 						type: 'damage',
-						damage: 6,
+						damage: 4,
 						targetSpec: {
 							scope: 'single',
 							side: 'enemy',
-							entityType: 'minion'
+							entityType: "all"
 						},
 					}
 				]
@@ -228,20 +228,52 @@ const cards: Card[] = [
 		id: 13,
 		color: 'black',
 		name: 'Dark Blessing',
-		description: 'Power freely given always has a price. Usually your weekends.',
+		description: 'Give a friendly minion +3 +2',
 		baseCost: 2,
 		type: 'incantation',
 		image_url: '',
-		abilities: []
+		abilities: [
+			{
+				trigger: 'onPlay',
+				effects: [
+					{
+						type: "buff",
+						attack: 3,
+						defence: 2,
+						conditions: [],
+						targetSpec: {
+							scope: "single",
+							side: "friendly",
+							entityType: "minion",
+						}
+					}
+				]
+			}
+		]
 	},
 	{
 		id: 14,
 		color: 'black',
 		name: 'Soul Drain',
-		description: 'Rips the life force from a target. Not covered by insurance.',
-		baseCost: 3,
+		description: 'Deal 2 damage',
+		baseCost: 1,
 		type: 'incantation',
 		image_url: '',
-		abilities: []
+		abilities: [
+			{
+				trigger: 'onPlay',
+				effects: [
+					{
+						type: 'damage',
+						damage: 2,
+						targetSpec: {
+							scope: 'single',
+							side: 'enemy',
+							entityType: "all"
+						},
+					}
+				]
+			}
+		]
 	}
 ];
