18d80221c46f4e377736a90a25ab68435b739bdb
freddeadredemption <fh@finrod.dk>
2026-04-19 19:04:52 +0200

    public url

--- client/src/lib/shared/cards.ts
+++ client/src/lib/shared/cards.ts
@@ -1,8 +1,9 @@
 import type { IncantationCard } from "./bindings/IncantationCard";
 import type { MinionCard } from "./bindings/MinionCard";
+import { PUBLIC_SERVER_URL } from '$env/static/public';
 
 export const getCards = async (): Promise<(MinionCard | IncantationCard)[]> => {
-    const res = await fetch('http://localhost:3000/cards');
+    const res = await fetch(`${PUBLIC_SERVER_URL}/cards`);
     const data = await res.json();
     return data.map((c: any) => {
         if ('Minion' in c) return { ...c.Minion, type: 'minion' };
