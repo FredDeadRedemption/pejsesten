517831c683e1c283f928476a806c8dc2fc47e1bb
freddeadredemption <fh@finrod.dk>
2026-04-21 16:23:47 +0200

    prod settings

--- server/src/settings.rs
+++ server/src/settings.rs
@@ -1,18 +1,15 @@
 #![allow(unused)]
 #![allow(dead_code)]
 
-pub const STARTING_HP: i32 = 100;
-pub const STARTING_MANA: i32 = 10;
+const PROD_STARTING_HP: i32 = 30;
+const PROD_STARTING_MANA: i32 = 1;
+const PROD_STARTING_HAND_SIZE: usize = 3;
+const PROD_BOT_DELAY_MS: u64 = 750;
+
+
+pub const STARTING_HP: i32 = if cfg!(debug_assertions) { 100 } else { PROD_STARTING_HP };
+pub const STARTING_MANA: i32 = if cfg!(debug_assertions) { 10 } else { PROD_STARTING_MANA };
 pub const MAX_MANA: i32 = 10;
-pub const STARTING_HAND_SIZE: usize = 7;
-pub const BOT_DELAY_MS: u64 = 750;
+pub const STARTING_HAND_SIZE: usize = if cfg!(debug_assertions) { 7 } else { PROD_STARTING_HAND_SIZE };
+pub const BOT_DELAY_MS: u64 = if cfg!(debug_assertions) { 250 } else { PROD_BOT_DELAY_MS };
 pub const OPEN_CARDS: bool = true;
-
-// Default
-//pub const STARTING_HP: i32 = 30;
-//pub const STARTING_MANA: i32 = 1;
-//pub const MAX_MANA: i32 = 10;
-//pub const STARTING_HAND_SIZE: usize = 3;
-//pub const BOT_DELAY_MS: u64 = 250;
-//pub const OPEN_CARDS: bool = true;
-
