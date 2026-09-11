f1e3503d1a49078107eb5fe8b2f9c99c4a34f06e
freddeadredemption <fh@finrod.dk>
2026-09-11 21:46:51 +0200

    fix: cleanup settings.rs

--- server/src/settings.rs
+++ server/src/settings.rs
@@ -1,16 +1,44 @@
+const DEV_STARTING_HP: i32 = 100;
+const DEV_MAX_HP: i32 = 100;
+
+const DEV_STARTING_MANA: i32 = 10;
+const DEV_MAX_MANA: i32 = 10;
+
+const DEV_STARTING_EMBERS: i32 = 5;
+const DEV_MAX_EMBERS: i32 = 5;
+
+const DEV_STARTING_HAND_SIZE: usize = 7;
+const DEV_MAX_HAND_SIZE: usize = 10;
+const DEV_MAX_BOARD_SIZE: usize = 5;
+const DEV_MULLIGAN: bool = true;
+const DEV_OPEN_CARDS: bool = true;
+const DEV_BOT_DELAY_MS: u64 = 250;
+
 const PROD_STARTING_HP: i32 = 30;
+const PROD_MAX_HP: i32 = 30;
+
 const PROD_STARTING_MANA: i32 = 1;
+const PROD_MAX_MANA: i32 = 10;
+
+const PROD_STARTING_EMBERS: i32 = 1;
+const PROD_MAX_EMBERS: i32 = 5;
+
 const PROD_STARTING_HAND_SIZE: usize = 3;
+const PROD_MAX_HAND_SIZE: usize = 10;
+const PROD_MAX_BOARD_SIZE: usize = 5;
+const PROD_MULLIGAN: bool = true;
+const PROD_OPEN_CARDS: bool = false;
 const PROD_BOT_DELAY_MS: u64 = 750;
-const PROB_MULLIGAN: bool = true;
 
-
-pub const STARTING_HP: i32 = if cfg!(debug_assertions) { 100 } else { PROD_STARTING_HP };
-pub const STARTING_MANA: i32 = if cfg!(debug_assertions) { 10 } else { PROD_STARTING_MANA };
-pub const MAX_MANA: i32 = 10;
-pub const STARTING_EMBERS: i32 = 1;
-pub const MAX_EMBERS: i32 = 5;
-pub const STARTING_HAND_SIZE: usize = if cfg!(debug_assertions) { 7 } else { PROD_STARTING_HAND_SIZE };
-pub const BOT_DELAY_MS: u64 = if cfg!(debug_assertions) { 250 } else { PROD_BOT_DELAY_MS };
-// pub const OPEN_CARDS: bool = true;
-pub const MULLIGAN: bool = if cfg!(debug_assertions) { true } else { PROB_MULLIGAN };
+pub const STARTING_HP: i32 = if cfg!(debug_assertions) { DEV_STARTING_HP } else { PROD_STARTING_HP };
+pub const MAX_HP: i32 = if cfg!(debug_assertions) { DEV_MAX_HP } else { PROD_MAX_HP };
+pub const STARTING_MANA: i32 = if cfg!(debug_assertions) { DEV_STARTING_MANA } else { PROD_STARTING_MANA };
+pub const MAX_MANA: i32 = if cfg!(debug_assertions) { DEV_MAX_MANA } else { PROD_MAX_MANA };
+pub const STARTING_EMBERS: i32 = if cfg!(debug_assertions) { DEV_STARTING_EMBERS } else { PROD_STARTING_EMBERS };
+pub const MAX_EMBERS: i32 = if cfg!(debug_assertions) { DEV_MAX_EMBERS } else { PROD_MAX_EMBERS };
+pub const STARTING_HAND_SIZE: usize = if cfg!(debug_assertions) { DEV_STARTING_HAND_SIZE } else { PROD_STARTING_HAND_SIZE };
+pub const MAX_HAND_SIZE: usize = if cfg!(debug_assertions) { DEV_MAX_HAND_SIZE } else { PROD_MAX_HAND_SIZE };
+pub const MAX_BOARD_SIZE: usize = if cfg!(debug_assertions) { DEV_MAX_BOARD_SIZE } else { PROD_MAX_BOARD_SIZE };
+pub const MULLIGAN: bool = if cfg!(debug_assertions) { DEV_MULLIGAN } else { PROD_MULLIGAN };
+pub const OPEN_CARDS: bool = if cfg!(debug_assertions) { DEV_OPEN_CARDS } else { PROD_OPEN_CARDS };
+pub const BOT_DELAY_MS: u64 = if cfg!(debug_assertions) { DEV_BOT_DELAY_MS } else { PROD_BOT_DELAY_MS };
