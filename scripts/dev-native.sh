9d3e8de8794b308b216de79df5ccdcb3eb6d228d
freddeadredemption <fh@finrod.dk>
2026-09-11 21:55:46 +0200

    chore: drop cargo-watch from dev script

--- scripts/dev-native.sh
+++ scripts/dev-native.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Native dev loop: server + auto-restarting macroquad client.
+# Native dev loop: server in the background, client in the foreground.
 set -e
 cd "$(dirname "$0")/.."
 
@@ -8,7 +8,6 @@ SERVER_PID=$!
 trap "kill $SERVER_PID 2>/dev/null" EXIT
 
 echo "server: pid $SERVER_PID, log: /tmp/pejsesten-server.log"
-echo "client: cargo-watch will rebuild and restart on save"
 echo
 
-exec cargo watch -x 'run -p client'
+cargo run -p client
